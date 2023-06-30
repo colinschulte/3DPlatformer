cdfa5c65a24fbef2d554ef29b766e0172c04f625
Colin <colingschulte@gmail.com>
2023-06-30 12:33:09 -0500

    Shrank overworld size

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,7 +11,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        DontDestroyOnLoad(gameObject);
+        //DontDestroyOnLoad(gameObject);
     }
 
     // Update is called once per frame
